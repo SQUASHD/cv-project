4bb774d790f3007e7835042ee0edd43fee5c8715
Daniel Hjartland <squashical@gmail.com>
2022-11-07 13:19:48 +0100

    Create more component modules and write basic construction

---
+++ src/Components/Education.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import EducationItem from "./EducationItem";
+
+class Education extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Education</h1>
+      </div>
+    );
+  }
+}
+
+export default Education;
