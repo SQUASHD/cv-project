c832d9c76c8d2f00173e1d3bacc07331f52b08ad
Daniel Hjartland <squashical@gmail.com>
2022-11-08 16:11:03 +0100

    Struggle through learning react

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
-// import ExperienceItem from "./ExperienceItem";
+import ExperienceItem from "./ExperienceItem";
 
 class Experience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      experience: [],
+    };
+  }
   render() {
+
     return (
       <div>
         <h1>Experience</h1>
