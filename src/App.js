b479dec2fec8cf40a219ea29709154170e5af8e8
Daniel Hjartland <squashical@gmail.com>
2022-11-09 13:32:31 +0100

    Create a basic CV form and preview for personal info with some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React, { Component } from "react";
-// import Info from "./Components/Info";
-// import Experience from "./Components/Experience";
-// import Education from "./Components/Education";
+import React, { Component } from 'react';
+import CVPreview from './Components/CVPreview';
+import CVForm from './Components/CVForm';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      personalDetails: {
+        name: '',
+        title: '',
+        email: '',
+        phone: '',
+        address: '',
+        city: '',
+      },
+      // experience: [],
+      // education: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState((prevState) => ({
+      personalDetails: {
+        ...prevState.personalDetails,
+        [e.target.name]: e.target.value,
+      },
+    }));
+  };
+
   render() {
-    return <div></div>;
+    const { name, title, email, phone, address, city } =
+      this.state.personalDetails;
+    return (
+      <div className="App">
+        <div className="cv-containers">
+          <div className="cv-builder-container">
+            <CVForm
+              name={name}
+              title={title}
+              email={email}
+              phone={phone}
+              address={address}
+              city={city}
+              handleChange={this.handleChange}
+            />
+          </div>
+          <div className="cv-preview-container">
+            <CVPreview
+              name={name}
+              title={title}
+              email={email}
+              phone={phone}
+              address={address}
+              city={city}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/Components/CVForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './CVForm.css';
+
+class CVForm extends Component {
+  render() {
+    const { name, title, email, phone, address, city, handleChange } =
+      this.props;
+    return (
+      <div className="cv-form">
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleChange}
+        />
+        <input
+          name="title"
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={handleChange}
+        />
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          name="phone"
+          type="text"
+          placeholder="Phone"
+          value={phone}
+          onChange={handleChange}
+        />
+        <input
+          name="address"
+          type="text"
+          placeholder="Address"
+          value={address}
+          onChange={handleChange}
+        />
+        <input
+          name="city"
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default CVForm;
