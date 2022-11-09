d3acb243b0ce3ae1feb68be90e50849fd5a800d5
Daniel Hjartland <squashical@gmail.com>
2022-11-09 07:52:00 +0100

    Remove all files and start from scratch

--- src/App.js
+++ src/App.js
@@ -4,22 +4,6 @@ import React, { Component } from "react";
 // import Education from "./Components/Education";
 
 class App extends Component {
-  constructor(props = {}) {
-    super(props);
-    this.state = {
-      info: {
-        firstName: '',
-        lastName: '',
-        Title: '',
-        Email: '',
-        Phone: '',
-        Address: '',
-      },
-      experience: [],
-      education: [],
-    };
-  }
-
   render() {
     return <div></div>;
   }
